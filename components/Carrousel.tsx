b85f83c8806f11d518c8e4dbf2f4171ba5b151cf
Ale <dicsuresh@gmail.com>
2025-02-14 01:44:25 -0300

    nuevos cambios

--- components/Carrousel.tsx
+++ components/Carrousel.tsx
@@ -1,4 +1,3 @@
-
 interface Image {
   src: string;
   alt: string;
@@ -12,9 +11,9 @@ interface CarrouselProps {
 
 export default function Carrousel({ images, selectedImage, onSelectImage }: CarrouselProps) {
   return (
-    <div className="flex flex-col items-center space-y-4 bg-white rounded-xl shadow-2xl p-4">
+    <div className="flex flex-col items-center space-y-4 bg-white rounded-xl shadow-2xl p-4 w-full max-w-4xl mx-auto">
       {/* Imagen principal */}
-      <div className="relative max-[768px]:w-3/4 min-[768px]:w-3/4 rounded-lg shadow-lg border-2 border-emerald-500 overflow-hidden">
+      <div className="relative w-full max-w-lg rounded-lg shadow-lg border-2 border-emerald-500 overflow-hidden">
         <img
           src={selectedImage}
           alt="Funko seleccionado"
@@ -23,11 +22,11 @@ export default function Carrousel({ images, selectedImage, onSelectImage }: Carr
       </div>
 
       {/* Previsualizaciones de imágenes */}
-      <div className="flex space-x-4">
+      <div className="w-full overflow-x-auto flex space-x-2 md:space-x-4 py-2 scrollbar-hide">
         {images.map((imagen, index) => (
           <div
             key={index}
-            className={`w-32 h-32 rounded-lg shadow-md border-2 cursor-pointer ${
+            className={`w-20 h-20 md:w-24 md:h-24 rounded-lg shadow-md border-2 cursor-pointer flex-shrink-0 transition-all duration-300 ease-in-out transform hover:scale-105 ${
               selectedImage === imagen.src ? "border-emerald-500" : "border-gray-300"
             }`}
             onClick={() => onSelectImage(imagen.src)}
@@ -35,7 +34,7 @@ export default function Carrousel({ images, selectedImage, onSelectImage }: Carr
             <img
               src={imagen.src}
               alt={imagen.alt}
-              className="object-cover rounded-lg"
+              className="w-full h-full object-cover rounded-lg"
             />
           </div>
         ))}
